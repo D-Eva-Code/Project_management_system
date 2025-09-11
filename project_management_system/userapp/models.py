506c606a73b6d23eead1dd6258c961df153a5f8f
D-Eva-Code <anagwonyeadaeze42@gmail.com>
2025-09-11 19:14:58 +0100

    added fields for users based on role selected

--- project_management_system/userapp/models.py
+++ project_management_system/userapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLE_CHOICES=(
+        'student', 'Student'
+        'supervisor', 'Supervisor'
+    )
+    role= models.CharField(max_length= 20, choices= ROLE_CHOICES)
+
+    staff_id= models.CharField(max_length=20)
+    matric_number= models.CharField(max_length=20)
+    department= models.CharField(max_length= 30)
+
+    supervisor= models.ForeignKey(
+        'self',
+        on_delete= models.SET_NULL,
+        limit_choices_to= {'role':'supervisor'},
+        related_name= 'students'
+    )
