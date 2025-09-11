ce5fd9ef29634942fd15f9b13bc93528efe051dc
D-Eva-Code <anagwonyeadaeze42@gmail.com>
2025-09-11 18:18:48 +0100

    added in forms form validation and conditions for when a role is selected

--- project_management_system/userapp/forms.py
+++ project_management_system/userapp/forms.py
@@ -8,4 +8,21 @@ class userform(UserCreationForm):
     # Name= forms.CharField(max_length=100)
     class Meta:
         model= User
-        fields=['username','email', 'password1', 'password2']
+        fields=['username','email', 'password1', 'password2', 'role', 'matricnumber', 'department','staff_id', 'supervisor']
+
+    def clean(self):
+        cleaned_data= super().clean()
+        role= cleaned_data.get('role')
+
+        if role== "student" and not cleaned_data.get("matric_number"):
+            self.add_error("matric number", "Student must enter matric number")
+
+        if role== "student" and not cleanded_data.get("supervisor"):
+            self.add_error("matric number", "Student must select a supervisor")
+
+        if role== "superisor" and not cleaned_data.get("staff_id"):
+            self.add_error("staff_id", "Supervisor must enter staff ID")
+
+        
+
+        return cleaned_data
--- project_management_system/userapp/urls.py
+++ project_management_system/userapp/urls.py
@@ -4,5 +4,5 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns=[
     path('register', views.register, name='register'),
-    path('login', auth_views.LoginView.as_view, name='login'),
+    path('login', auth_views.LoginView.as_view(template_name= 'login.html'), name='login'),
 ]
