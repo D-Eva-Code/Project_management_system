7593e731cb7563e1a1ee3da74ff2315519143cf8
D-Eva-Code <anagwonyeadaeze42@gmail.com>
2025-09-11 16:50:41 +0100

    updated login

--- project_management_system/project_management_system/settings.py
+++ project_management_system/project_management_system/settings.py
@@ -131,5 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK="bootstrap5"
 
-LOGIN_REDIRECT_URL='login.html'
-LOGIN_URL='login'
+LOGIN_REDIRECT_URL="login.html"
+LOGIN_URL="login"
